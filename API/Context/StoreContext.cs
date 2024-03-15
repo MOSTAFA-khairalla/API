7a4bf3f3506cc3ad2ac3b0fd41672a0d6746785c
Elhamd Pc <Elhamd Pc@DESKTOP-NB8KSO3>
2024-03-15 22:55:03 +0200

    Add project files.

---
+++ API/Context/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Enities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Context
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductController:ControllerBase
+    {
+
+        [HttpGet]
+
+        public string GetProducts()
+        {
+
+            return "hello mostafa";
+        }
+
+        [HttpGet("id")]
+        public string GetProducts(int id)
+        {
+            return "Hello Mostafa khairalla" + id;
+        }
+    }
+}
